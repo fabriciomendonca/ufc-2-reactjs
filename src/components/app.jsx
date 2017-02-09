60e73490f18de6d58b5cbfecc1fa73ffcd75d269
Fabricio Mendonca <fabriciomendonca@gmail.com>
2017-02-09 17:38:39 -0200

    App component and main render

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Simple datatable app</h1>
+      </div>
+    );
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+const elm = <App />;
+
+ReactDOM.render(elm, document.getElementById('root'));
