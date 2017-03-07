91e92c13ed13173ef71c376b23310e011fdc9509
Fabricio Mendonca Rodrigues <fabriciomendonca@Fabricios-MacBook-Pro.local>
2017-03-07 12:11:33 -0300

    Call GitHub API

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount() {
+    const URL = 'https://api.github.com/users';
+
+    axios.get(URL).then((response) => {
+      this.setState({
+        users: response.data
+      })
+    });
+  }
+
+  renderList() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.users.map((user) => {
+            return (
+              <tr key={user.id}><td>{user.login}</td></tr>
+            )
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
+    const list = this.state.users ? this.renderList() : '<div>Loading users...</div>';
+
     return(
       <div>
         <h1>Simple datatable app</h1>
+        {list}
       </div>
     );
   }
